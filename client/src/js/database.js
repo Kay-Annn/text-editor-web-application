7fedc000db855b3d33d537509b797b40b4b10e48
KAY-ANN WILLIAMS <56706010+Kay-Annn@users.noreply.github.com>
2023-02-06 00:05:47 -0500

    Update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,7 +27,6 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
   const jateDb = await openDB('jate', 1);
 
   const tx = jateDb.transaction('jate', 'readonly');
